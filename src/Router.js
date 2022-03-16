16680c777440767d0dad3f25c4be7c0f9f4f78ce
Timberzzman <leloupandco@gmail.com>
2022-03-16 17:52:39 +0100

    Adding bad routes images

--- src/Router.js
+++ src/Router.js
@@ -43,7 +43,7 @@ function Router() {
           </React.Suspense>
             )}
       />
-      <Route path="*" element={BadRoute} />
+      <Route path="*" element={<BadRoute />} />
     </Routes>
   );
 }
--- src/views/BadRoute.js
+++ src/views/BadRoute.js
@@ -2,8 +2,15 @@ import React from 'react';
 
 function BadRoute() {
   return (
-    <section>
-      <p>Bad route</p>
+    <section className="text-black dark:text-white">
+      <div className="mt-20">
+        <img className="px-10 md:px-0 mx-auto mb-10" src="https://giffiles.alphacoders.com/118/118777.gif" alt="lostMortys" />
+        <div className="text-3xl md:text-6xl">
+          <p>You shouldn&apos;t been here !</p>
+          <br />
+          <p>Go back to home, Loser !</p>
+        </div>
+      </div>
     </section>
   );
 }
